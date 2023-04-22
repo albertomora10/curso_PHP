26141f586fe440c462c28196ea99f1cf5501ff81
Alberto Mora <albertom-10@hotmail.com>
2023-04-22 23:27:33 +0200

    Delete contact

--- delete.php
+++ delete.php
@@ -5,10 +5,10 @@ require "database.php";
 
 $id = $_GET["id"];
 
-$statement = $conn->prepare("SELECT FROM contacts WHERE id = :id");
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
 $statement->execute([":id" => $id]);
 
-if ($statement -> rowCount() == 0) {
+if ($statement->rowCount() == 0) {
   http_response_code(404);
   echo("HTTP 404 NOT FOUND");
   return;
